4f3948ef516e4dd441b441fc40de6263a16cfa28
Nisarul Amin Naim <65971192+nisarul-media@users.noreply.github.com>
2023-01-13 03:56:58 +0600

    Version 2.0

--- nisarul-investment-counter.php
+++ nisarul-investment-counter.php
@@ -1,13 +1,44 @@
 <?php
 /*
 Plugin Name: Investment Counter by Nisarul
-Plugin URI: https://nisarul.com/
-Description: Use shortcode [nisarul_investment_counter_string starting-at="1045200000" initial-time="1673382993"] to generate Comma separated number,  and [nisarul_investment_counter_time starting-at='1045200000' initial-time='1673382993'] to generate non-comma separated number.
+Plugin URI: https://github.com/nisarul-media/Investment-Counter-WordPress-Plugin
+Description: Use shortcode [nisarul_investment_counter_string starting_time='2023-01-11 00:00:00' starting_money='1045200000' increasing_rate='1'] to generate Comma separated number,  and [nisarul_investment_counter_time starting_time='2023-01-11 00:00:00' starting_money='1045200000' increasing_rate='1'] to generate non-comma separated number.
 Author: Nisarul Amin Naim
 Author URI: https://www.fiverr.com/nisarul/
 Text Domain:nisarul-investment-counter
-Version: 1.0
+Version: 2.0
 */
+
+/**
+ * Summary of investment_counter_fnc
+ * @param mixed $starting_time
+ * @param mixed $starting_money
+ * @param mixed $increasing_rate
+ * @return mixed
+ */
+function investment_counter_fnc( $starting_time, $starting_money, $increasing_rate ) {
+	$time_in_sec = 0;
+
+	$starting_time_obj = new DateTime( $starting_time );
+	$current_time_obj = new DateTime();
+
+	$diff_time_obj = $starting_time_obj->diff( $current_time_obj );
+
+	if ( $diff_time_obj->invert ) {
+		return (int) $starting_money;
+	} else {
+		if ( $diff_time_obj->days != 0 )
+			$time_in_sec = $diff_time_obj->days * 24 * 60 * 60;
+		if ( $diff_time_obj->h != 0 )
+			$time_in_sec += $diff_time_obj->h * 60 * 60;
+		if ( $diff_time_obj->i != 0 )
+			$time_in_sec += $diff_time_obj->i * 60;
+		if ( $diff_time_obj->s != 0 )
+			$time_in_sec += $diff_time_obj->s;
+	}
+
+	return (int) ( $starting_money + ( $increasing_rate * $time_in_sec ) );
+}
 
 /**
  * Generates Comma separated string
@@ -17,14 +48,22 @@ Version: 1.0
  */
 function investment_counter_string( $atts, $content ) {
 	$atts = shortcode_atts( [
-		'initial-time'     => '1673382993',
-        'starting-at' => '1045200000'
+		'starting_time'   => '2023-01-11 00:00:00',
+		'starting_money'  => '1045200000',
+		'increasing_rate' => '1',
 	] , $atts );
+	extract( $atts );
 
-	$left_time = time() - (int) $atts['initial-time'];
-	$return_time = (int) $atts['starting-at'] + $left_time;
-	$return_string = number_format( $return_time, 0, '.', ',' );
-	return $return_string;
+	return number_format(
+		investment_counter_fnc(
+			$starting_time,
+			$starting_money,
+			$increasing_rate
+		),
+		0,
+		'.',
+		',',
+	);
 }
 add_shortcode( 'nisarul_investment_counter_string', 'investment_counter_string' );
 
@@ -36,12 +75,12 @@ add_shortcode( 'nisarul_investment_counter_string', 'investment_counter_string' 
  */
 function investment_counter_time($atts, $content) {
 	$atts = shortcode_atts( [
-		'initial-time' => '1673382993',
-		'starting-at' => '1045200000'
+		'starting_time' => '2023-01-11 00:00:00',
+		'starting_money' => '1045200000',
+		'increasing_rate' => '1',
 	], $atts );
+	extract( $atts );
 
-	$left_time = time() - (int) $atts['initial-time'];
-	$return_time = (int) $atts['starting-at'] + $left_time;
-	 return $return_time;
+	return investment_counter_fnc( $starting_time, $starting_money, $increasing_rate );
 }
 add_shortcode( 'nisarul_investment_counter_time', 'investment_counter_time' );
